ade1dec97e11536ae1b66633daff891c0d0f0777
florinagalben <florina.galben@bitstone.com>
2025-10-29 14:57:38 +0200

    Close button added to menu and Antrenori

--- HoopDreams/wwwroot/js/site.js
+++ HoopDreams/wwwroot/js/site.js
@@ -2,3 +2,17 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+document.addEventListener('DOMContentLoaded', function () {
+    var navbarToggler = document.querySelector('.navbar-toggler');
+    var navbarClose = document.querySelector('.navbar-close');
+    var navbarCollapse = document.querySelector('.navbar-collapse');
+
+    if (navbarClose && navbarToggler && navbarCollapse) {
+        navbarClose.addEventListener('click', function () {
+            if (navbarCollapse.classList.contains('show')) {
+                navbarToggler.click();
+            }
+        });
+    }
+});
